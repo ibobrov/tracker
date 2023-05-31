0d752035b6d0002a697bca563e910e5e33d54115
ibobrov <coderilya@gmail.com>
2023-06-01 02:54:14 +1000

    Extend tracker functions

--- src/main/java/i/bobrov/tracker/Tracker.java
+++ src/main/java/i/bobrov/tracker/Tracker.java
@@ -1,22 +1,66 @@
 package i.bobrov.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
     public Item add(Item item) {
-        item.setId(ids++);
-        items[size++] = item;
+        if (item != null) {
+            item.setId(ids++);
+            items[size++] = item;
+        }
         return item;
     }
 
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int size = 0;
+        for (Item i : items) {
+            if (i != null && key.equals(i.getName())) {
+                result[size++] = i;
+            }
+        }
+        return Arrays.copyOf(result, size);
+    }
+
     public Item findById(int id) {
-        Item rsl = null;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
+    }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        boolean rsl = index != -1 && item != null;
+        if (rsl) {
+            item.setId(id);
+            items[index] = item;
+        }
+        return rsl;
+    }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        boolean rsl = index != -1;
+        if (rsl) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
+        return rsl;
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
         for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
+            if (items[index].getId() == id) {
+                rsl = index;
                 break;
             }
         }
