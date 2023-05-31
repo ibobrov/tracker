f6c450d8ebd5ea371d5f948725761d74138da392
ibobrov <coderilya@gmail.com>
2023-05-31 21:43:30 +1000

    Add Start UI, field created from Item

---
+++ src/main/java/i/bobrov/tracker/StartUI.java
@@ -0,0 +1,14 @@
+package i.bobrov.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        LocalDateTime created = item.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        System.out.println(created.format(formatter));
+    }
+}
