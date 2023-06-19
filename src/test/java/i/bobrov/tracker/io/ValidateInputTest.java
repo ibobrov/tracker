c8cf916e05f1e416a23de7a18f6661dea6c0408b
ibobrov <coderilya@gmail.com>
2023-06-19 13:48:58 +1000

    Fix ValidateInputTest

--- src/test/java/i/bobrov/tracker/io/ValidateInputTest.java
+++ src/test/java/i/bobrov/tracker/io/ValidateInputTest.java
@@ -9,7 +9,7 @@ class ValidateInputTest {
     public void whenInvalidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"one", "-"}
+                new String[] {"one", "1"}
         );
         ValidateInput input = new ValidateInput(out, in);
         int selected = input.askInt("Enter menu:");
